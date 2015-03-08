a3b6b1e4f49910e5b2d73252c6d76f18b8ced7b1
Evan Sosenko <razorx@evansosenko.com>
2015-03-07 20:52:51 -0800

    Customize variables.

--- settings/variables.lua
+++ settings/variables.lua
@@ -4,13 +4,13 @@ os.setlocale(os.getenv("LANG"))
 -- common
 modkey     = "Mod4"
 altkey     = "Mod1"
-terminal   = "urxvtc" or "xterm"
+terminal   = "terminator" or "xterm"
 editor     = os.getenv("EDITOR") or "nano" or "vi"
 editor_cmd = terminal .. " -e " .. editor
 
 -- user defined
-browser    = "dwb"
-browser2   = "iron"
+browser    = "firefox"
+browser2   = "google-chrome-stable"
 gui_editor = "gvim"
 graphics   = "gimp"
 mail       = terminal .. " -e mutt "
